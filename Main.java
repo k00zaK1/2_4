ee375b0a35d276c1fdd63dbaf1d72717eefec1f4
k00zaK1 <127319229+k00zaK1@users.noreply.github.com>
2023-03-21 21:56:59 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+  System.out.print("Wybierz opcję : ");
+  System.out.print("1 : suma ");
+  System.out.print("2 : roznica ");
+  System.out.print("3 : iloczyn ");
+  System.out.print("4 : iloraz ");
+
+    
+  }
+}
