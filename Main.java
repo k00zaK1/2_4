7f57420411d433bb09fda45d74f6ce5ebae9c8ea
k00zaK1 <127319229+k00zaK1@users.noreply.github.com>
2023-03-21 22:31:36 +0000

    asdf

--- Main.java
+++ Main.java
@@ -5,12 +5,45 @@ public class Main {
 
     Scanner scanner = new Scanner(System.in);
 
-  System.out.print("Wybierz opcję : ");
-  System.out.print("1 : suma ");
-  System.out.print("2 : roznica ");
-  System.out.print("3 : iloczyn ");
-  System.out.print("4 : iloraz ");
-
+  System.out.println("Wybierz opcję : ");
+  System.out.println("1  suma ");
+  System.out.println("2  roznica ");
+  System.out.println("3  iloczyn ");
+  System.out.println("4  iloraz ");
+  int wybor =  scanner.nextInt();
+    if (wybor < 1 || wybor > 4){
+      System.out.println("Spierdalaj ");
+     return;
+    }
+  System.out.print("Podaj liczbę a: ");
+  double a = scanner.nextDouble();
+  System.out.print("Podaj liczbę b: ");
+  double b = scanner.nextDouble();
     
+    switch(wybor){
+      case 1:
+        double suma = a + b;
+        System.out.println("Suma wynosi " +suma);
+    
+        break;
+      case 2:
+        double roznica = a - b;
+        System.out.println("Roznica wynosi " +roznica);
+        break;
+      case 3:
+        double iloczyn = a * b;
+        System.out.println("Iloczyn wynosi " +iloczyn);
+        break;
+      case 4:
+        if(b == 0 ) {
+        System.out.println("Nie dziel przez zero!");
+        }else{
+          double iloraz = a / b;
+          System.out.println("Iloraz wynosi " +iloraz);
+          break;
+          
+    
+        }
+    }
   }
 }
